4b79c931fa8812de4145c4cf5334471c00a9c7e2
Omura Shunsuke <omurashunsuke@Omuras-MacBook-Pro.local>
2019-04-25 18:19:31 +0900

    first commit

---
+++ video.js
@@ -0,0 +1,14 @@
+//動画流す準備
+var video = document.getElementById("video");
+// getUserMedia によるカメラ映像の取得
+var media = navigator.mediaDevices.getUserMedia({
+  video: true, //ビデオを取得する
+  //使うカメラをインカメラか背面カメラかを指定する場合には
+  //video: { facingMode: "environment" },//背面カメラ
+  //video: { facingMode: "user" },//インカメラ
+  audio: false //音声が必要な場合はture
+});
+//リアルタイムに再生（ストリーミング）させるためにビデオタグに流し込む
+media.then(stream => {
+  video.srcObject = stream;
+});
